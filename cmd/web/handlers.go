3d12a7bedf152bba9e8112ee2ecd457493d3b001
rezyte <alirezaalavirazavi@yahoo.com>
2022-11-15 23:45:57 +0330

    3.3 - Dependacy injection and exporting in go modules

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,42 +1,39 @@
-package main 
+package main
 
 import (
 	"fmt"
-	"strconv"
-	"net/http"
 	"html/template"
-	"log"
+	"net/http"
+	"strconv"
 )
 
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
+	// Include the footer partial in the template files.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
 
-func home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        http.NotFound(w, r)
-        return
-    }
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.errorLog.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
 
-    // Include the footer partial in the template files.
-    files := []string{
-        "./ui/html/home.page.tmpl",
-        "./ui/html/base.layout.tmpl",
-        "./ui/html/footer.partial.tmpl",
-    }
-
-    ts, err := template.ParseFiles(files...)
-    if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-        return
-    }
-
-    err = ts.Execute(w, nil)
-    if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-    }
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.errorLog.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
@@ -44,14 +41,13 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-
-	// We’re able to do pass in w to Fprintf function the io.Writer type is 
-	// an interface, and the http.ResponseWriter object satisfies the 
+	// We’re able to do pass in w to Fprintf function the io.Writer type is
+	// an interface, and the http.ResponseWriter object satisfies the
 	// interface because it has a w.Write() method.
 	fmt.Fprintf(w, "Display a specific snippet with %s", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "POST" {
 		// WriteHeader is only possible to call once per response
